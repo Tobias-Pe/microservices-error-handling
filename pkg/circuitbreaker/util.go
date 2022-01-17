2ea615c119ae6b2a26480d5b1762c86d547ca569
Tobias Peslalz <peslalz.tobias@hm.edu>
2022-01-17 10:00:15 +0100

    :sparkles: adjust circuit breaker params

--- pkg/circuitbreaker/util.go
+++ pkg/circuitbreaker/util.go
@@ -11,9 +11,11 @@ func NewCircuitBreaker(name string, cbMetric *metrics.CircuitBreakerMetric) *gob
 	st.Name = name
 	st.ReadyToTrip = func(counts gobreaker.Counts) bool {
 		failureRatio := float64(counts.TotalFailures) / float64(counts.Requests)
-		return counts.Requests >= 10 && failureRatio >= 0.25
+		return counts.Requests >= 50 && failureRatio >= 0.25
 	}
 	st.Timeout = time.Duration(3) * time.Second
+	st.MaxRequests = 25
+	st.Interval = time.Duration(10) * time.Second
 	st.OnStateChange = func(_ string, from gobreaker.State, to gobreaker.State) {
 		cbMetric.Update(from, to, name)
 	}
